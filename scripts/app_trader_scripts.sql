5cf9e60319414d05ce5ab15a95952e9d063be3a5
Ryan Hartman <ryanhartman247@gmail.com>
2024-06-20 18:00:42 -0500

    v1

--- scripts/app_trader_scripts.sql
+++ scripts/app_trader_scripts.sql
@@ -23,7 +23,7 @@
 -- ORDER BY 2 DESC, 1 ASC
 
 
-
+-- couldn't get this to work
 -- SELECT
 -- 	name,
 -- 	CASE
@@ -38,54 +38,151 @@
 -- 	USING(name)
 -- ORDER BY 2 DESC
 	
---Haylie's code:
-SELECT 
-	name, 
-	MAX(maxprice)
-FROM (
-	SELECT name, price AS maxprice
-	FROM app_store_apps AS asa
-	UNION
-	SELECT name, CAST(TRIM('$' from price) AS numeric) AS maxprice
-	FROM play_store_apps AS psa
-	) AS subquery
-GROUP BY name
-
--- SELECT
--- 	CAST(TRIM('$' FROM p.price) AS numeric) + a.price
--- FROM app_store_apps AS a
--- JOIN play_store_apps AS p
--- USING(name)
 
 
--- SELECT
---     CAST(REPLACE(REPLACE(TRIM(p.price), '$', ''), ',', '') AS numeric) + CAST(REPLACE(REPLACE(TRIM(a.price), '$', ''), ',', '') AS numeric) AS total_price
--- FROM app_store_apps AS a
--- JOIN play_store_apps AS p
--- USING(name);
 
-	
+WITH 
+subquery1 AS (  -- calculates max_price, avg_rating, monthly_revenue, and app store availability
+	SELECT
+	      COALESCE(a.name, p.name) AS app_name -- returns the first non null value, so either name is fine since they are the same 
+		, GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric))::MONEY AS max_price -- returns whichever price is greater  
+		, CASE -- returns avg rating when rating in both tables, or rating when in one table 
+			WHEN (a.rating IS NULL AND p.rating IS NOT NULL) THEN p.rating -- do we actually need to round all these? The math still holds..
+			WHEN (p.rating IS NULL AND a.rating IS NOT NULL) THEN a.rating 
+			WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN ROUND((a.rating + p.rating)/2, 1)
+			ELSE 0
+			END AS avg_rating
+		, CASE -- returns 5k revenue when on one store and 10k when on both stores 
+			WHEN (a.name IS NULL AND p.name IS NOT NULL) THEN 5000::MONEY
+			WHEN (p.name IS NULL AND a.name IS NOT NULL) THEN 5000::MONEY
+			WHEN (a.name IS NOT NULL AND p.name IS NOT NULL) THEN 10000::MONEY
+			ELSE NULL
+			END AS monthly_revenue
+		, CASE -- returns which stores the app is available on 
+			WHEN (a.name IS NULL AND p.name IS NOT NULL) THEN 'PLAY store only'
+			WHEN (p.name IS NULL AND a.name IS NOT NULL) THEN 'APP store only'
+			WHEN (a.name = p.name) THEN 'both'
+			ELSE NULL
+			END AS app_availability
+	FROM app_store_apps a
+	FULL JOIN play_store_apps p 
+		ON a.name = p.name
+	),
+subquery2 AS (  -- calculates purchase_price (by building on max_price), projected_lifespan (by building on avg_rating), monthly_income (by building on monthly_revenue). 
+	SELECT
+		app_name
+		, CASE -- calculates purchase price 
+			WHEN max_price <= 1::MONEY THEN 10000::MONEY
+			ELSE (max_price * 10000)::MONEY
+	 		END AS purchase_price
+		, (avg_rating * 2) + 1 AS projected_lifespan
+		, monthly_revenue - 1000::MONEY AS monthly_income  -- is the math right here?? 
+	FROM app_store_apps a
+	FULL JOIN play_store_apps p 
+		ON a.name = p.name
+	FULL JOIN subquery1 s1
+		ON a.name = s1.app_name
+	)
+SELECT
+      DISTINCT s2.app_name
+	, app_availability
+	, max_price
+	, avg_rating
+	, projected_lifespan
+	, purchase_price
+	, monthly_revenue
+	, monthly_income
+	, monthly_income * projected_lifespan - purchase_price AS total_income -- is this math correct? 
+FROM subquery1 AS s1
+FULL JOIN subquery2 AS s2 
+	ON s1.app_name = s2.app_name
+--WHERE app_availability = 'PLAY store only'
+ORDER BY total_income DESC
 
 
-	
-	
--- FROM app_store_apps a
--- FULL JOIN play_store_apps p
+-- sean's code: 
+-- WITH data_list AS
+-- 	(SELECT
+-- 		name
+-- 		,CASE
+-- 				WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN 2
+-- 				ELSE 1 END AS number_stores
+-- 		,CASE
+-- 				WHEN (a.rating IS NULL AND p.rating IS NOT NULL) THEN ROUND(ROUND(p.rating * 2) / 2, 1)
+-- 				WHEN (p.rating IS NULL AND a.rating IS NOT NULL) THEN ROUND(ROUND(a.rating * 2) / 2, 1)
+-- 				WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN ROUND(ROUND(((a.rating+p.rating)/2) * 2) / 2, 1)
+-- 				ELSE '0'
+-- 				END AS avg_rating
+-- 		,CASE
+-- 				WHEN (a.rating IS NULL AND p.rating IS NOT NULL) THEN ((ROUND(ROUND(p.rating * 2) / 2, 1)) + .5) * 2
+-- 				WHEN (p.rating IS NULL AND a.rating IS NOT NULL) THEN ((ROUND(ROUND(a.rating * 2) / 2, 1)) + .5) * 2
+-- 				WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN ((ROUND(ROUND(((a.rating+p.rating)/2) * 2) / 2, 1))+.5) * 2
+-- 				ELSE '1'
+-- 				END AS lifespan_years
+-- 		,GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric))::MONEY AS app_price
+-- 		,CASE
+-- 				WHEN GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric)) = 0 THEN (GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric))+1 * 10000)::MONEY
+-- 				ELSE (GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric))* 10000)::MONEY END AS purchase_price
+-- 	FROM app_store_apps a
+-- 	FULL JOIN play_store_apps p
+-- 	USING(name)
+-- 	)
+-- SELECT *
+-- 	, (lifespan_years * 12000)::MONEY AS lifespan_marketing_cost
+-- 	, ((lifespan_years * 60000)* number_stores)::MONEY AS lifespan_revenue
+-- 	, ((lifespan_years * 60000)* number_stores)::MONEY - (lifespan_years * 12000)::MONEY AS lifespan_profit
+-- FROM data_list
+-- ORDER BY lifespan_profit DESC
+
+
+
+
+
+
+
+-- old code..
+-- WITH subquery1 AS (  -- subquery does calculations so main query can call on them 
+-- 	SELECT
+-- 	    COALESCE(a.name, p.name) AS app_name -- returns the first non null value, so either name is fine since they are the same 
+-- 	    , GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric)) AS max_price -- returns largest value from set  
+-- 		, CASE -- returns avg rating when rating in both tables, or rating when in one table 
+-- 			WHEN (a.rating IS NULL AND p.rating IS NOT NULL) THEN p.rating
+-- 			WHEN (p.rating IS NULL AND a.rating IS NOT NULL) THEN a.rating
+-- 			WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN ROUND((a.rating + p.rating)/2,1)
+-- 			ELSE NULL
+-- 			END AS avg_rating 	
+-- 		, CASE -- returns 5k revenue when on one store and 10k when on both stores 
+-- 			WHEN (a.name IS NULL AND p.name IS NOT NULL) THEN 5000::MONEY
+-- 			WHEN (p.name IS NULL AND a.name IS NOT NULL) THEN 5000::MONEY
+-- 			WHEN (a.name IS NOT NULL AND p.name IS NOT NULL) THEN 10000::MONEY
+-- 			ELSE NULL
+-- 			END AS monthly_revenue
+-- 	FROM app_store_apps a
+-- 	FULL JOIN play_store_apps p 
 -- 	ON a.name = p.name
+-- 	)
+-- SELECT
+--     app_name
+-- 	, max_price
+-- 	, avg_rating
+-- 	, (max_price * 10000)::MONEY AS purchase_price
+-- 	, monthly_revenue
+-- FROM subquery1 AS s1
+-- ORDER BY 2 DESC
 
 
--- Select 
--- 	name, 
--- 	-- COALESCE(ROUND(AVG((PLAY.rating + APP.rating)/2), 2), 0) as combined_rating,
--- 	CASE
--- 		WHEN APP.name IS NOT NULL AND THEN APP.rating
--- 		WHEN PLAY.name != 0 THEN PLAY.rating
-	
--- FROM app_store_apps APP
--- FULL JOIN play_store_apps PLAY
--- USING(name)
--- GROUP BY name
--- ORDER BY combined_rating DESC
+
+-- simple code to test 
+-- SELECT
+-- 	COALESCE(a.name, p.name) as ap_name,
+-- 	*
+-- 	-- CAST(TRIM('$' FROM p.price) AS numeric) + a.price
+-- FROM app_store_apps AS a
+-- FULL OUTER JOIN play_store_apps AS p
+-- 	ON a.name = p.name
+-- WHERE p.name = 'Eu Sou Rico'
+
+
 
 
 
