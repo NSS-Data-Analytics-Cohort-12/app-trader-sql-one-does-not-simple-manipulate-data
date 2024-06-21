c08adf889820d5a9853d672cc7bc45cdbe3c2d47
Ryan Hartman <ryanhartman247@gmail.com>
2024-06-20 20:51:32 -0500

    final?

--- scripts/app_trader_scripts.sql
+++ scripts/app_trader_scripts.sql
@@ -47,9 +47,9 @@ subquery1 AS (  -- calculates max_price, avg_rating, monthly_revenue, and app st
 	      COALESCE(a.name, p.name) AS app_name -- returns the first non null value, so either name is fine since they are the same 
 		, GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric))::MONEY AS max_price -- returns whichever price is greater  
 		, CASE -- returns avg rating when rating in both tables, or rating when in one table 
-			WHEN (a.rating IS NULL AND p.rating IS NOT NULL) THEN p.rating -- do we actually need to round all these? The math still holds..
-			WHEN (p.rating IS NULL AND a.rating IS NOT NULL) THEN a.rating 
-			WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN ROUND((a.rating + p.rating)/2, 1)
+			WHEN (a.rating IS NULL AND p.rating IS NOT NULL) THEN ROUND(ROUND(p.rating * 2) / 2, 1)
+			WHEN (p.rating IS NULL AND a.rating IS NOT NULL) THEN ROUND(ROUND(a.rating * 2) / 2, 1) 
+			WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN ROUND(ROUND(((a.rating+p.rating)/2) * 2) / 2, 1)
 			ELSE 0
 			END AS avg_rating
 		, CASE -- returns 5k revenue when on one store and 10k when on both stores 
@@ -77,6 +77,7 @@ subquery2 AS (  -- calculates purchase_price (by building on max_price), project
 	 		END AS purchase_price
 		, (avg_rating * 2) + 1 AS projected_lifespan
 		, monthly_revenue - 1000::MONEY AS monthly_income  -- is the math right here?? 
+		, CAST(a.review_count AS integer) + p.review_count AS total_review_count
 	FROM app_store_apps a
 	FULL JOIN play_store_apps p 
 		ON a.name = p.name
@@ -84,7 +85,7 @@ subquery2 AS (  -- calculates purchase_price (by building on max_price), project
 		ON a.name = s1.app_name
 	)
 SELECT
-      DISTINCT s2.app_name
+      DISTINCT (s2.app_name) 
 	, app_availability
 	, max_price
 	, avg_rating
@@ -92,12 +93,18 @@ SELECT
 	, purchase_price
 	, monthly_revenue
 	, monthly_income
-	, monthly_income * projected_lifespan - purchase_price AS total_income -- is this math correct? 
+	, monthly_income*12 * projected_lifespan - purchase_price AS total_income  
+	--, SUM(total_review_count) OVER(PARTITION BY s2.app_name) AS sum_total_review_count
+ 	--, total_review_count
 FROM subquery1 AS s1
 FULL JOIN subquery2 AS s2 
 	ON s1.app_name = s2.app_name
---WHERE app_availability = 'PLAY store only'
-ORDER BY total_income DESC
+WHERE s2.app_name IS NOT NULL 
+	AND total_review_count IS NOT NULL
+	--AND s2.app_name = 'Instagram'
+ORDER BY total_income DESC --, sum_total_review_count DESC
+LIMIT 152
+
 
 
 -- sean's code: 
@@ -140,47 +147,22 @@ ORDER BY total_income DESC
 
 
 
--- old code..
--- WITH subquery1 AS (  -- subquery does calculations so main query can call on them 
--- 	SELECT
--- 	    COALESCE(a.name, p.name) AS app_name -- returns the first non null value, so either name is fine since they are the same 
--- 	    , GREATEST(a.price, CAST(TRIM('$' FROM p.price) AS numeric)) AS max_price -- returns largest value from set  
--- 		, CASE -- returns avg rating when rating in both tables, or rating when in one table 
--- 			WHEN (a.rating IS NULL AND p.rating IS NOT NULL) THEN p.rating
--- 			WHEN (p.rating IS NULL AND a.rating IS NOT NULL) THEN a.rating
--- 			WHEN (a.rating IS NOT NULL AND p.rating IS NOT NULL) THEN ROUND((a.rating + p.rating)/2,1)
--- 			ELSE NULL
--- 			END AS avg_rating 	
--- 		, CASE -- returns 5k revenue when on one store and 10k when on both stores 
--- 			WHEN (a.name IS NULL AND p.name IS NOT NULL) THEN 5000::MONEY
--- 			WHEN (p.name IS NULL AND a.name IS NOT NULL) THEN 5000::MONEY
--- 			WHEN (a.name IS NOT NULL AND p.name IS NOT NULL) THEN 10000::MONEY
--- 			ELSE NULL
--- 			END AS monthly_revenue
--- 	FROM app_store_apps a
--- 	FULL JOIN play_store_apps p 
--- 	ON a.name = p.name
--- 	)
--- SELECT
---     app_name
--- 	, max_price
--- 	, avg_rating
--- 	, (max_price * 10000)::MONEY AS purchase_price
--- 	, monthly_revenue
--- FROM subquery1 AS s1
--- ORDER BY 2 DESC
-
-
 
 -- simple code to test 
 -- SELECT
--- 	COALESCE(a.name, p.name) as ap_name,
--- 	*
+-- 	DISTINCT COALESCE(a.name, p.name) as ap_name,
+-- 	SUM(CAST(a.review_count AS integer)) AS app_review,
+-- 	SUM(p.review_count) AS play_review,
+-- 	SUM(CAST(a.review_count AS integer) + p.review_count) AS combined_review_count
 -- 	-- CAST(TRIM('$' FROM p.price) AS numeric) + a.price
 -- FROM app_store_apps AS a
 -- FULL OUTER JOIN play_store_apps AS p
 -- 	ON a.name = p.name
--- WHERE p.name = 'Eu Sou Rico'
+-- --WHERE p.name = 'Eu Sou Rico'
+-- WHERE a.rating IS NOT NULL AND CAST(a.review_count AS integer) + p.review_count IS NOT NULL
+-- GROUP BY 1
+-- ORDER BY combined_review_count DESC
+
 
 
 
